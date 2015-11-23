e0f6c2eb13068540f0f3ff88331fb56c7b4a5be3
Rahul Garg <rahgarg@cisco.com>
2015-11-23 12:42:55 -0800

    done bubble sort with some unittesting.

---
+++ bubble_sort.py
@@ -0,0 +1,53 @@
+# placeholder for bubble sort
+import unittest
+
+
+class Bubbler(object):
+    def __init__(self, sample):
+        ''' sample is the list to sort
+        '''
+        self.sample = sample
+
+    @property
+    def sorter(self):
+        ''' implement bubble sort here
+        '''
+        inp = self.sample
+        sorted = False
+        while not sorted:
+            sorted = True
+            for i in xrange(len(inp)-1):
+                if inp[i] > inp[i+1]:
+                    sorted = False
+                    inp[i+1], inp[i] = inp[i], inp[i+1]
+        return inp
+
+    def __repr__(self):
+        return 'Bubbler({})'.format(self.sample)
+
+
+class BubblerTestCases(unittest.TestCase):
+
+    def test_simple(self):
+        x1 = [1, 2, 3, 4, 5]
+        b = Bubbler(x1)
+        self.assertEquals(b.sorter, sorted(b.sample))
+
+    def test_negatives(self):
+        x2 = [4, 5, 23, 5, 6, 1, 6, -1, 0.3]
+        b = Bubbler(x2)
+        self.assertEquals(b.sorter, sorted(b.sample))
+
+    def test_all_negatives(self):
+        x3 = [-1, -4, -3, -2, -5]
+        b = Bubbler(x3)
+        self.assertEquals(b.sorter, sorted(b.sample))
+
+    def test_decimals(self):
+        x4 = [0.3, 0.2, 0.1, 0.4, -0.5, -0.3]
+        b = Bubbler(x4)
+        self.assertEquals(b.sorter, sorted(b.sample))
+
+
+if __name__ == '__main__':
+    unittest.main()
